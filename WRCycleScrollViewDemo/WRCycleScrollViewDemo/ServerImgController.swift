2192bf9faa5d5ccd93849d9b85ead1f9024760ea
wangrui460 <wangruidev@gmail.com>
2017-05-13 15:27:52 +0800

    测试不显示desc

--- WRCycleScrollViewDemo/WRCycleScrollViewDemo/ServerImgController.swift
+++ WRCycleScrollViewDemo/WRCycleScrollViewDemo/ServerImgController.swift
@@ -22,12 +22,7 @@ class ServerImgController: UIViewController
                             "http://p.lrlz.com/data/upload/mobile/special/s303/s303_05442007587372591.png",
                             "http://p.lrlz.com/data/upload/mobile/special/s303/s303_05442007388249407.png",
                             "http://p.lrlz.com/data/upload/mobile/special/s303/s303_05442007470310935.png"]
-        let descs = ["韩国防部回应停止部署萨德:遵照最高统帅指导方针",
-                     "勒索病毒攻击再次爆发 国内校园网大面积感染",
-                     "Win10秋季更新重磅功能：跟安卓与iOS无缝连接",
-                     "《琅琊榜2》为何没有胡歌？胡歌：我看过剧本，离开是种保护",
-                     "阿米尔汗在印度的影响力，我国的哪位影视明星能与之齐名呢？"]
-        let cycleScrollView = WRCycleScrollView(frame: frame, type: .SERVER, imgs: serverImages, descs:descs)
+        let cycleScrollView = WRCycleScrollView(frame: frame, type: .SERVER, imgs: serverImages)
         view.addSubview(cycleScrollView)
     }
 }
